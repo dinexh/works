c9f018ee079275d070359c26acf767818b082595
dinesh <dineshkorukonda05@gmail.com>
2025-10-07 10:39:32 +0530

    added basic page

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+const Home = () => {
+  return(
+    <div className="home-component flex flex-col items-center justify-center min-h-screen bg-black text-white gap-4">
+      <h1 className="text-4xl font-bold">Works by Dinesh Korukonda</h1>
+      <h3 className="text-xl">Waiting for launching....</h3>
+    </div>
+  );
+};
+export default Home;
