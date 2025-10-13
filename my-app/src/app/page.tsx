b50b91723750b17298c1bc57501a2246b8c0e5ab
dineshkorukonda <dineshkorukonda@dineshkorukondas-MacBook-Air.local>
2025-10-13 16:49:04 +0530

    added link

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -5,6 +5,7 @@ const Home = () => {
     <div className="home-component flex flex-col items-center justify-center min-h-screen bg-black text-white gap-4">
       <h1 className="text-4xl font-bold">Works by Dinesh Korukonda</h1>
       <h3 className="text-xl">Waiting for launching....</h3>
+        <h3 className="text-x">visit https://dineshkorukonda.in
     </div>
   );
 };
